cd44601147ddb30254486042667022adc0d80c6d
Rasmus <rasmus.marttila@hotmail.com>
2020-11-16 03:47:21 +0200

    Tietokannan alku olemassa

--- app.py
+++ app.py
@@ -1,34 +1,15 @@
 from flask import Flask
-from flask import render_template, request
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///rasmus"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    words = ["motiivi", "tilaisuus", "keinot"]
-    return render_template("template1.html",message="MURHA",items=words)
-
-@app.route("/page1")
-def page1():
-    return "Kokeilu"
-
-@app.route("/test")
-def test():
-    content = ""
-    for i in range(1,101):
-        content += str(i)+" "
-    return content
-
-@app.route("/page/<int:id>")
-def page(id):
-    return "Tämä on sivu "+str(id) + "\n" + str(2*(id))
-
-@app.route("/form")
-def form():
-    return render_template("form.html")
-
-@app.route("/result", methods=["POST"])
-def result():
-    return render_template("result.html",name=request.form["name"])
-
+    result = db.session.execute("SELECT COUNT(*) FROM artist")
+    count = result.fetchone()[0]
+    result = db.session.execute("SELECT (name, id) FROM artist")
+    artistit = result.fetchall()
+    return render_template("index.html", count=count, artistit=artistit) 
